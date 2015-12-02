8562e00d6d7c567bc93ec1c728883f2b3a58407b
DavyVan <affqqa@163.com>
2015-12-02 22:29:01 +0800

    Enable constant memory and shared memory

--- PiOnGPU/main.cu
+++ PiOnGPU/main.cu
@@ -17,23 +17,26 @@
 
 #include<memory>
 #include<iostream>
+#include<cstdlib>
 #include<cassert>
 
 #include<cuda_runtime.h>
 #include <device_launch_parameters.h>
 #include"CUDA Helper\helper_cuda.h"
-#include "functions.h"
-#include "kernel.h"
+//#include "functions.h"
+//#include "kernel.h"
 
+#define ENABLE_CONSTANT_MEM
+#define ENABLE_SHARED_MEM
 
 #define dLEAP 2
 #define PRECISION_INIT 2
-#define PRECISION_HEX 1008	//of digits in hex
-#define PRECISION_OUTPUT_START 1000
-#define PRECISION_OUTPUT_END 1008
+#define PRECISION_HEX 20008	//of digits in hex
+#define PRECISION_OUTPUT_START (PRECISION_HEX - 8)
+#define PRECISION_OUTPUT_END (PRECISION_HEX)
 
 #define WARP_SIZE 32
-#define WARPS_PER_BLOCK 32	//1024 threads per block
+#define WARPS_PER_BLOCK 2	//1024 threads per block
 #define THREADS_PER_BLOCK ((WARPS_PER_BLOCK) * (WARP_SIZE))
 #define BLOCKS_NUM ((PRECISION_HEX/dLEAP/THREADS_PER_BLOCK) + 1)
 
@@ -61,14 +64,27 @@ __device__ __int64 biexp(__int64 a, __int64 b, __int64 mod)
 }
 
 char hex_table1[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+#ifndef ENABLE_CONSTANT_MEM
 char *hex_table = NULL;
+#else ENABLE_CONSTANT_MEM
+__constant__ char hex_table[16];
+#endif
+
 __host__ void hex_table_init()
 {
+#ifndef ENABLE_CONSTANT_MEM
 	checkCudaErrors(cudaMalloc((void **) &hex_table, 16 * sizeof(char)));
 	checkCudaErrors(cudaMemcpy(hex_table, hex_table1, 16 * sizeof(char), cudaMemcpyHostToDevice));
+#else
+	checkCudaErrors(cudaMemcpyToSymbol(hex_table, hex_table1, 16 * sizeof(char)));
+#endif
 }
 
-__device__ void DecToHexModDiv(unsigned char dec, char &hex, char* hex_table)
+__device__ void DecToHexModDiv(unsigned char dec, char &hex
+#ifndef ENABLE_CONSTANT_MEM
+	, char* hex_table
+#endif
+	)
 {
 	hex = hex_table[dec];
 }
@@ -89,9 +105,23 @@ __global__ void PiOnGPU_Kernel(
 	__int64 precision_hex,
 	__int64 dLeap,
 	unsigned char *result_dec,
-	char *result_hex,
-	char *hex_table)
+	char *result_hex
+#ifndef ENABLE_CONSTANT_MEM
+	,char *hex_table
+#endif
+	)
 {
+#ifdef ENABLE_SHARED_MEM
+	__shared__ unsigned char shared_result_dec[THREADS_PER_BLOCK * dLEAP];
+	__shared__ char shared_result_hex[THREADS_PER_BLOCK * dLEAP];
+	for (int i = threadIdx.x; i < threadIdx.x + dLeap; i++)
+	{
+		shared_result_dec[i] = 0;
+		shared_result_hex[i] = 0;
+	}
+
+	__syncthreads();
+#endif
 	__int64 tid = (blockIdx.x * blockDim.x) + threadIdx.x;
 	__int64 precision_current = precision_init + tid * dLeap;
 	if (precision_current >= precision_hex-1)
@@ -101,14 +131,44 @@ __global__ void PiOnGPU_Kernel(
 	_16dPi = 4 * F16dSj(precision_current, 1) - 2 * F16dSj(precision_current, 4) - F16dSj(precision_current, 5) - F16dSj(precision_current, 6);
 	_16dPi = (_16dPi > 0) ? (_16dPi - (int) _16dPi) : (_16dPi - (int) _16dPi + 1);
 
+#ifndef ENABLE_SHARED_MEM
 	for (__int64 i = precision_current; i < precision_current + dLeap; i++)
 	{
 		_16dPi *= 16;
 		result_dec[i] = (int) _16dPi;
 		_16dPi -= result_dec[i];
 
-		DecToHexModDiv(result_dec[i], result_hex[i], hex_table);
+		DecToHexModDiv(result_dec[i], result_hex[i]
+#ifndef ENABLE_CONSTANT_MEM
+			, hex_table
+#endif
+			);
 	}
+#else
+	for (__int64 i = threadIdx.x * dLeap; i < threadIdx.x * dLeap + dLeap; i++)
+	{
+		_16dPi *= 16;
+		shared_result_dec[i] = (int) _16dPi;
+		_16dPi -= shared_result_dec[i];
+
+		DecToHexModDiv(shared_result_dec[i], shared_result_hex[i]
+#ifndef ENABLE_CONSTANT_MEM
+			, hex_table
+#endif
+			);
+	}
+
+	__syncthreads();
+
+	if (threadIdx.x == 0)
+	{
+		for (__int64 i = precision_current; i < precision_current + THREADS_PER_BLOCK * dLeap; i++)
+		{
+			result_dec[i] = shared_result_dec[i - precision_current];
+			result_hex[i] = shared_result_hex[i - precision_current];
+		}
+	}
+#endif
 }
 
 
@@ -223,7 +283,11 @@ int main(int argc, char **argv)
 	checkCudaErrors(cudaMalloc((void **) &result_hex_gpu, (PRECISION_HEX + 1) * sizeof(unsigned char)));
 
 	checkCudaErrors(cudaEventRecord(start));
-	PiOnGPU_Kernel<<<BLOCKS_NUM, THREADS_PER_BLOCK>>>(PRECISION_INIT, PRECISION_HEX, dLEAP, result_dec_gpu, result_hex_gpu, hex_table);
+	PiOnGPU_Kernel<<<BLOCKS_NUM, THREADS_PER_BLOCK>>>(PRECISION_INIT, PRECISION_HEX, dLEAP, result_dec_gpu, result_hex_gpu
+#ifndef ENABLE_CONSTANT_MEM
+		, hex_table
+#endif
+		);
 	checkCudaErrors(cudaEventRecord(stop));
 
 	checkCudaErrors(cudaEventSynchronize(stop));
@@ -240,6 +304,6 @@ int main(int argc, char **argv)
 	printf("Total Time: %f\n", elapseTime);
 
 	cudaDeviceReset();
-	getchar();
+	//system("pause");
 	exit(EXIT_SUCCESS);
 }
